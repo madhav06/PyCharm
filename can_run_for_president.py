6227ebbfed6cce0f6a62f75b94871460fd47760a
Madhav Nandan <mnandan06@gmail.com>
2020-04-13 15:45:35 +0530

    booleans

---
+++ can_run_for_president.py
@@ -0,0 +1,9 @@
+#using boolean operator find the following
+
+def can_run_for_president(age):
+    """Can someone of the given age run for president in the US?"""
+    # The US Constitution says you must "have attained to the Age of thirty-five Years"
+    return age >= 35
+
+print("Can a 19-year-old run for president?", can_run_for_president(19))
+print("Can a 45-year-old run for president?", can_run_for_president(45))
