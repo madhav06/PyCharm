395fce3a606b894a64610d4dc9c7e350e74cab03
Madhav Nandan <mnandan06@gmail.com>
2020-04-13 15:33:21 +0530

    functions

---
+++ fun2.py
@@ -0,0 +1,9 @@
+
+#define function
+
+def my_fun(usrname,greeting):
+    print("Hello, %s, From Utah!, I wish you %s"%(usrname,greeting))
+
+##print(greetings)
+print(" ")
+my_fun("John Doe", "a good morning")
