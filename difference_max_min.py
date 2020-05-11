0e3f51c2ce636713acc862ac6d3ae0e46be29538
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 19:21:09 +0530

    edabit algorithms

---
+++ difference_max_min.py
@@ -0,0 +1,18 @@
+# python 3
+
+# Create a function that takes a list and returns the difference between the biggest and smallest numbers.
+
+# Examples
+# difference_max_min([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+# # Smallest number is -50, biggest is 32.
+
+# difference_max_min([44, 32, 86, 19]) ➞ 67
+# # Smallest number is 19, biggest is 86.
+
+def difference_max_min(lst):
+	return abs(max(lst)-min(lst))
+
+if __name__=='__main__':
+ print(difference_max_min([10, 4, 1, 4, -10, -50, 32, 21]))
+ print(difference_max_min([44, 32, 86, 19]))
+
