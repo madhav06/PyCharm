6d7620d17aeedac48eaabeedc07db8c8f9f438dd
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 07:25:47 +0530

    area of a triangle

---
+++ tri_area.py
@@ -0,0 +1,16 @@
+# Python 3
+
+# tri_area(3, 2) -> 3
+
+# tri_area(7, 4) -> 14
+
+# tri_area(10, 2) -> 10
+
+
+def tri_area(base,height):
+	tri_area = ((base*height) / 2)
+	print(tri_area)
+
+tri_area(7, 4)
+
+tri_area(10, 2)
