3e250c17a4033020de2cb3bb22e4b717baa48e3b
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 07:34:21 +0530

    EdaBit: convert mins to secs

---
+++ convertTosec.py
@@ -0,0 +1,19 @@
+
+# Python 3
+
+
+
+# convert(5) ➞ 300
+
+# convert(3) ➞ 180
+
+# convert(2) ➞ 120
+
+def convert(minutes):
+	seconds = minutes * 60
+	print(seconds)
+
+
+convert(5)
+convert(3)
+convert(2)
