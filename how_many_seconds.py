2a32945fe8f344e27ba9434851031848286c8539
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 07:46:16 +0530

    edabit challenges

---
+++ how_many_seconds.py
@@ -0,0 +1,11 @@
+
+#Python 3
+
+def how_many_seconds(hour):
+	seconds = hour * 3600
+	print(seconds)
+
+
+
+how_many_seconds(2)
+how_many_seconds(10)
---
+++ next_num.py
@@ -0,0 +1,18 @@
+# python 3
+
+
+
+# addition(0) ➞ 1
+
+# addition(9) ➞ 10
+
+# addition(-3) ➞ -2
+
+def next_num(int):
+	next_num = int + 1
+	print(next_num)
+
+
+next_num(0)
+next_num(9)
+next_num(-3)
