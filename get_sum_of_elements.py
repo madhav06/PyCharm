90fd99ed6311eb0f17ee18f487f1792dbc507289
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 13:30:11 +0530

    edabit challenges

---
+++ get_sum_of_elements.py
@@ -0,0 +1,21 @@
+
+# Python 3
+
+# Create a function that takes a list and returns the sum of all numbers in the list.
+
+# get_sum_of_elements([2, 7, 4]) ➞ 13
+
+# get_sum_of_elements([45, 3, 0]) ➞ 48
+
+# get_sum_of_elements([-2, 84, 23]) ➞ 105
+
+
+def get_sum_of_elements(lst):
+ #return sum(lst)
+ print(sum(lst))
+ 
+get_sum_of_elements([2, 7, 4]) 
+
+get_sum_of_elements([45, 3, 0]) 
+
+get_sum_of_elements([-2, 84, 23])
