8ceb34d85fee257ffebde48be47273fd1d5ab773
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 13:09:47 +0530

    edabit challenges

---
+++ less_than_100.py
@@ -0,0 +1,24 @@
+# Python3
+
+# Given two numbers, return True if the sum of both numbers is less than 100. 
+# Otherwise return False.
+
+
+
+#less_than_100(22, 15) ➞ True
+# 22 + 15 = 37
+
+#less_than_100(83, 34) ➞ False
+# 83 + 34 = 117
+
+def less_than_100(a, b):
+ if(a+b<100):
+  #return True
+  print(True)
+ else:
+  #return False
+  print(False)
+	
+less_than_100(22, 15)
+
+less_than_100(83, 34)
