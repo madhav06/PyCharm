8f401d2251d7eb6a89aaecfed8cb4a826ef3deba
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 08:01:06 +0530

    edabit challenges

---
+++ get_first_value.py
@@ -0,0 +1,15 @@
+# Python 3
+
+# Create a function that takes a list and returns the first element.
+
+
+# get_first_value([1, 2, 3]) ➞ 1
+
+# get_first_value([80, 5, 100]) ➞ 80
+
+# get_first_value([-500, 0, 50]) ➞ -500
+
+
+def get_first_value(number_list):
+	get_first_value = number_list[0]
+	return get_first_value
