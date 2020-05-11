b62eca83ffeef9acb5e46cc42c2a6f42c3ed4ac4
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-11 12:42:51 +0530

    edabit challenges

---
+++ squared.py
@@ -0,0 +1,22 @@
+# Python 3
+
+#Fix the code in the code tab to pass this challenge (only syntax errors). 
+# create a code that output square of input.
+
+
+#squared(5) ➞ 25
+
+#squared(9) ➞ 81
+
+#squared(100) ➞ 10000
+
+def squared(b):
+	#return (b * b)
+	print( b * b)
+
+
+squared(5) 
+
+squared(9) 
+
+squared(100)
