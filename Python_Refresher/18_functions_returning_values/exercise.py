a466bb104caa4b7310e73e2ec7a8abf8f0703ab9
MADHAV NANDAN <mnandan06@gmail.com>
2020-07-17 19:42:26 +0530

    coding exercise

---
+++ Python_Refresher/18_functions_returning_values/exercise.py
@@ -0,0 +1,8 @@
+# Complete the function by making sure it returns 42.
+def return_42():
+    return 42
+
+# Create a function below, called my_function, that takes two argumwnts and 
+# returns the result of its arguments multiplied together.
+def my_function(arg1, arg2):
+    return arg1 *  arg2
